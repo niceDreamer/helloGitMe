87d5303705356a67d7d5be404dadc711e3ee8496
master <nicedreamer@qq.com>
2012-10-16 18:29:26 +0800

    [Add] add flag file.

---
+++ master.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * $comment
+ * @copyright	Copyright(nicedreamer@qq.com) 2012
+ * @author		liuwei.yang<nicedreamer@qq.com>
+ * @created		2012-10-13
+ * @changed		2012-10-13
+ * @version		1.0
+ */
+?>
